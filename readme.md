90cc7a976bb1860795651811228a3c54b88c9086
veronikaostrovski <veronika.turkey@gmail.com>
2023-06-27 18:47:03 +0300

    Task 1, task 2, task 3, task 4, task 5 done

---
+++ readme.md
@@ -0,0 +1,32 @@
+1.simpleParamPositiveTestGet
+
+| Number | Check name  | Status |        
+|--------|-------------|--------|
+| 1      | 1           | Passed |
+| 2      | 5           | Passed |
+| 3      | 10          | Passed |
+
+1.simpleParamNegativeTestGet
+
+| Number | Check name | Status |        
+|--------|------------|--------|
+| 1      | 0          | Passed |
+| 2      | 11         | Passed |
+
+2. simplePositiveTestPost 
+
+| Number | Check name | Status |        
+|--------|------------|--------|
+| 1      | 200 Ok     | Passed |
+
+3. simpleNegativeTestPost
+
+| Number | Check name                 | Status |        
+|--------|----------------------------|--------|
+| 1      | 415 Unsupported Media Type | Passed |
+
+4. simplePositiveTestResponseBodyStatusOpen
+
+| Number | Check name    | Status |        
+|--------|---------------|--------|
+| 1      | Status "OPEN" | Passed |
---
+++ src/test/java/org/example/ApiTest.java
@@ -0,0 +1,113 @@
+package org.example;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApiTest {
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 10})
+    public void simpleParamPositiveTestGet(int id) {
+        given()
+                .when()
+                .log()
+                .all()
+                .get("http://51.250.6.164:8080/test-orders/{id}", id)
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 11})
+    public void simpleParamNegativeTestGet(int id) {
+        given()
+                .when()
+                .log()
+                .all()
+                .get("http://51.250.6.164:8080/test-orders/{id}", id)
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    String requestBody = "{\n" +
+            "  \"status\": \"INPROGRESS\",\n" +
+            "  \"courierId\": 12,\n" +
+            "  \"customerName\": \"string\",\n" +
+            "  \"customerPhone\": \"string\",\n" +
+            "  \"comment\": \"string\",\n" +
+            "  \"id\": 0\n" +
+            "}";
+
+    @Test
+    public void simplePositiveTestPost() {
+        given()
+                .when()
+                .header("Content-Type", "application/json")
+                .body(requestBody)
+                .log()
+                .all()
+                .post("http://51.250.6.164:8080/test-orders")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    String requestBody415 = "{\n" +
+            "  \"status\": \"DELIVERED\",\n" +
+            "  \"courierId\": 16,\n" +
+            "  \"customerName\": \"string\",\n" +
+            "  \"customerPhone\": \"string\",\n" +
+            "  \"comment\": \"string\",\n" +
+            "  \"id\": 0\n" +
+            "}";
+
+    @Test
+    public void simpleNegativeTestPost() {
+        given()
+                .when()
+                // .header("Content-Type", "application/json")
+                .body(requestBody415)
+                .log()
+                .all()
+                .post("http://51.250.6.164:8080/test-orders")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE);
+    }
+
+    @Test
+    public void simplePositiveTestResponseBodyStatusOpen() {
+
+        String status = given()
+                .when()
+                .log()
+                .all()
+                .get("http://51.250.6.164:8080/test-orders/7")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .extract()
+                .path("status");
+
+        Assertions.assertEquals("OPEN", status);
+
+
+    }
+}
+
+
